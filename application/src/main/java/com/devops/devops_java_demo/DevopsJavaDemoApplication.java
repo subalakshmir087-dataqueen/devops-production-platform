bcd3f56318ed2ed697f498a13ede0f09a302692d
Subalakshmi Rajendran <subalakshmirajendran25@gmail.com>
2026-06-28 13:21:42 +0530

    Add Spring Boot application

---
+++ application/src/main/java/com/devops/devops_java_demo/DevopsJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devops_java_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJavaDemoApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/devops/devops_java_demo/DevopsJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_java_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
